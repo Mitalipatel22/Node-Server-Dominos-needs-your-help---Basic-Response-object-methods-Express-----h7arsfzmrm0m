cb68a24453b32633f6d6d1f2dff66b98b054af07
Mitali Patel <104678425+Mitalipatel22@users.noreply.github.com>
2022-09-25 13:40:11 +0530

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  if(req.url=='/welcome'){
+      res.statusCode=200;
+      res.setHeader("Content-Type","text/plain");
+  res.end('Welcome to Dominos!');
+  }
+  else if(req.url=='/contact'){
+      const contactobj={
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+      }
+      res.statusCode=200;
+      res.setHeader("Content-Type","application/json");
+    res.end(JSON.stringify(contactobj));
+  }
+  else{
+res.writeHead(404).end("page not found");
+  }
+
 }
 
+console.log('listening for client request....');
+httpServer.listen(8081);
 module.exports = httpServer;
